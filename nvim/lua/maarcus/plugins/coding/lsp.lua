93c7b6d963dfb2c9fc4ccea2b70eed2adeb42118
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-02 10:55:10 +0530

    feat: update nvim configuration for coding

--- nvim/lua/maarcus/plugins/coding/lsp.lua
+++ nvim/lua/maarcus/plugins/coding/lsp.lua
@@ -1,5 +1,21 @@
 return {
 	{
+		"nvim-java/nvim-java",
+		config = false,
+		dependencies = {
+			{
+				"neovim/nvim-lspconfig",
+				opts = {
+					setup = {
+						jdtls = function()
+							require("java").setup({})
+						end,
+					},
+				},
+			},
+		},
+	},
+	{
 		"williamboman/mason.nvim",
 		lazy = false,
 		config = function()
@@ -27,9 +43,13 @@ return {
 
 			lspconfig.jdtls.setup({
 				capabilities = capabilities,
+				cmd = {
+					"jdtls",
+					"--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand("$MASON/share/jdtls/lombok.jar")),
+				},
 			})
 
-			lspconfig.tsserver.setup({
+			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
 
@@ -38,6 +58,14 @@ return {
 			})
 
 			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.angularls.setup({
 				capabilities = capabilities,
 			})
 		end,
