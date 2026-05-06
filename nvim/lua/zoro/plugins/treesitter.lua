5debb42832992405b5ee2ba69b2b8824ee70d52b
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2026-05-06 14:09:14 +0530

    feat(nvim): setup treesitter

---
+++ nvim/lua/zoro/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+
+    config = function()
+      require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        -- core
+        "lua",
+        "vim",
+        "vimdoc",
+        "bash",
+
+        -- web
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+
+        --config
+        "yaml",
+        "toml",
+        "dockerfile",
+        "gitignore",
+
+        -- databases
+        "sql",
+
+        -- markdown
+        "markdown",
+        "markdown_inline",
+
+        -- java
+        "java",
+
+        -- mobile
+        "swift",
+
+      },
+
+        highlight = {
+          enable = true,
+        },
+
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+}
