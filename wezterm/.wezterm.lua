54272cebe27386d796d59ca4f86ed5d8cf1c0025
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2026-05-06 08:46:19 +0530

    feat(wezterm): setup initial wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder();
+
+-- font
+config.font = wezterm.font 'GeistMono Nerd Font'
+config.font_size = 16
+
+-- theme
+config.color_scheme = 'Catppuccin Mocha'
+
+-- window
+config.window_background_opacity = 0.92
+config.window_padding = { left = 20, right = 20, top = 20, bottom = 20 }
+config.native_macos_fullscreen_mode = false
+config.window_decorations = 'RESIZE'
+
+-- cursor
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 700
+
+-- tabs
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = false
+
+-- scrollback 
+config.scrollback_lines = 1000
+
+-- term
+config.term = 'xterm-256color'
+
+-- leader
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+return config
