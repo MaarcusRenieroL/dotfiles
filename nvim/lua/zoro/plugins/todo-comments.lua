39e55cac8c72a5ad3987f7de61df08c79b669949
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2026-05-13 09:31:07 +0530

    feat(nvim): setup todo comments plugin

---
+++ nvim/lua/zoro/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("todo-comments").setup()
+
+		vim.keymap.set("n", "<leader>td", "<cmd>TodoTrouble<CR>")
+		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>")
+	end,
+}
